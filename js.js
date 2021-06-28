3e06572b28e52cfb9b05af9a8a55f88fd7a25757
Nebojsa Jeremic <neki333@gmail.com>
2021-06-28 23:45:24 +0200

    1st try

---
+++ js.js
@@ -0,0 +1,201 @@
+let qq = [
+    "1.Navedi vremenske okvire predmeta AD111 Istorija umetnosti starog i srednjeg veka.",
+    "2.Navedi najmanje tri epohe iz istorije kulture koje su obuhvaćene ovim redmetom.",
+    "3.Šta je karakteristično za arhajsku umetnost?",
+    "4.Navedi najpoznatije artefakte arhajske umetnosti.",
+    "5.Kako se zove haljina i ogrtač koju nose kore, statue devojaka, karakteristične skulpture antičkog perioda?",
+    "6.Koji vremenski period nazivamo periodom klasične grčke umetnosti?",
+    "7.Koliko redova postoji u grčkoj arhitekturi? Navedi njihova imena i odlike",
+    "8.Navedi najpoznatiji hram dorskog, jonskom i korintskog rada?",
+    "9.Kako se zove skulptor koji je radio dekoraciju hrama i skulpturu čuvene Atine Partenos?",
+    "10.Kako se naziva tehnika u kojoj je radjena skulptura Atine Partenos?",
+    "11.Kome je posvećen Partenon na Atinskom Akropolju?",
+    "12.Na kom mestu su Grci uobičajeno podizali hramove?",
+    "13.Kako se zove vrhovni bog grčke mitologije? Navedi imena vrhovnih grčkihbogova.",
+    "14.Navedi najpoznatije skulptore antičke grčke umetnosti, kao i njihova reprezentativna dela.",
+    "15.Šta je to kanon?",
+    "16.Koje su teme činile program metopa Partenona?",
+    "17.Gde je tradicionalno bilo mesto žene u staroj Grčkoj?",
+    "18.Šta znaš o helenizmu?",
+    "19.Kome je posvećen Pergamonski oltar?",
+    "20.Navedi primere helenističke skulpture.",
+    "21.Šta su sve rimljani gradili?",
+    "22.Šta znaš o Trajanovom stubu?",
+    "23.Šta je Panteon?",
+    "24.Šta su terme? Gde se nalaze terme u Beogradu?",
+    "25.Gde se nalazi Dioklecijanova palata?",
+    "26.Šta je to Ara Pacis?",
+    "27.Šta je to gema?",
+    "28.Šta su fajumski portreti i koja je bila njihova namena.",
+    "29.Šta su akvadukti?",
+    "30.Koji imperator ima ključnu ulogu u priznavanju hrišćanstva kao državne religije?",
+    "31.Šta je to judaizam?",
+    "32.Šta znaš o najstarijoj jevrejskoj sinagogi Dura Europosu.",
+    "33.Da li postoje sinagoge u Beogradu i Srbiji?",
+    "34.Šta predstavlja karakteristične oblike vizuelne kulture ranohrišćanskog perioda.",
+    "35.Navedi nekoliko ranohrišćanskih spomenika.",
+    "36.Šta su katakombe?",
+    "37.Šta je to vizantijska vizuelna kultura?",
+    "38.Navedi vremenske okvire vizantijske kulture i umetnosti.",
+    "39.Koja crkva predstavlja simbol vizantijske arhitekture?",
+    "40.Na kom konstruktivnom elementu počiva vizantijska kupola?",
+    "41.Koji car i carica su prikazani na čuvenom ravenskom mozaiku u crkvi San Vitale?",
+    "42.Šta je to ikonokalazam?",
+    "43.Šta je apsida? Šta je časna trpeza?",
+    "44.Kada nastaje islam i ko je osnivač ove religije?",
+    "45.Iz čega se sastoji sveto muslimansko hodočašće?",
+    "46.Šta je džamija?",
+    "47.Da li postoje figuralne predstave u islamskoj umetnosti? Gde se one nalaze?",
+    "48.Šta je mihrab?",
+    "49.Šta je mimbar?",
+    "50.Najpoznatija grobnica islamske arhitekture je...",
+    "51.Kada nastaje i za kog vladara se vezuje karolinška umetnost?",
+    "52.Koji je najpoznatiji arhitektornski spomenik karolinške arhitekture?",
+    "53.Šta znaš o ulozi knjige u karolinškoj umetnosti?",
+    "54.Vremenski okviri Otonske umetnosti",
+    "55.Koja je ključna gradjevina otonskog perioda?",
+    "56.Navedi vremenske okvire romaničke umetnosti",
+    "58. Šta je karakteristično za romaničku umetnost 11. i 12. veka",
+    "59. Navedi poznate romaničke manastire i katedrale u Francuskoj.",
+    "59.5. Najreprezentativnija romanička katedrala u Italji je...",
+    "60. Šta znaš o epohi gotike?",
+    "61.Šta znaš o funkciji gotičke katedrale?",
+    "62.Navedi različite medije u kojima se javlja slika u gotici.",
+    "63.Navedi italijanske umetnike predstavnike gotičkog slikarstva.",
+    "64.Kako se zove jedan od najstarijih hrišćanskih hramova u Srbiji? Kome je posvećen?",
+    "65.Navedi zadužbine Stefana Nemanje.",
+    "66.Navedi imena najstarijih srpskih pisanih i iluminiranih rukopisa?",
+    "68. Koji hram, manastir ili crkva je po tebi najreprezentativniji primer srpske umetnosti i arhitekture 13. ili 14. veka? Zašto.",
+    "69.Nabroj zadužbine kralja Milutina.",
+    "70.Navedi barem dva spomenika takozvane „moravske škole“",
+    "71.Ko je bio ktitor manastira Dečani?"
+];
+
+let aa = [
+    "Od 500. g.p.n.e. do 1500. G.n.e.",
+    "Antička Grčka, Rim, Jevrejska kultura, vizantijska umetnost, islamska umetnost, romanika, gotika.",
+    "Arhajski period, 7-5. v.p.n.e. Karakteristične forme umetnosti su skulpture muških i ženskih figura- kurosi i kore. Nage figure sa rukama uz telo i jednom nogom u iskoraku. Telo kora je prekriveno draperijom. Jače isticanje tema u grnčariji i vaze koje su manje veličine u odnosu na prethodni period. Arhajsko slikarstvo – karakteristična je jasna i ravnomerna boja.",
+    "Kurosi i kore, crnofiguralna i crvenofiguralna grnčarija.",
+    "Hiton i himation",
+    "Od 500. g.p.n.e. do smrti Aleksandra Velikog Makedonskog 323. G.p.n.e.7. Koliko redova postoji u grčkoj arhitekturi? Navedi njihova imena i odlike Dorski, jonski i korintski. Jonski i korintski stub imaju stopu, stablo i kapitel, dok dorski nema stopu i kracije. Stub je sastavljen iz više delova-tambura.",
+    "Dorski, jonski i korintski. Jonski i korintski stub imaju stopu, stablo i kapitel, dok dorski nema stopu i kracije. Stub je sastavljen iz više delova-tambura.",
+    "Dorski - Partenon, Jonski – Erehtejon, Korintski – Olimpejon",
+    "Fidija.",
+    "U hrisoelefantskoj tehnici.",
+    "Posvećen je grčkoj boginji Atini.",
+    "Na svetim brdima i planinama van gradova.",
+    "Vrhovni bog grčke mitologije – Zevs <br> Hera, Posejdon, Had, Atina, Hefest",
+    "Poliklet(Doriforos, Amazonka,hera), Miron(Diskobol), Lisip(Aristotel, Aleksandar Veliki), Praksitel (Afrodita Knidosa, Hermes od Praksitelesa)",
+    "Kanon je prvi poznati tekst u Grčkoj. Napisao ga je Poliklet i zasnovan je na konceptu lepote koja se ogleda u proporcijama ljudskog tela. Visina glave iznosi jednu sedminu visine tela, a šaka jednu desetinu i širina tela u ramenima odgovara širini tri glave. Bitne stavke u kanonu su simetrija proporcija i harmonija.",
+    "Istočne metope – gigantomahija, Zapadne metope – amazonomahija, Južne metope – Kentauromahija, Severne metope – teme iz Trojanskog rata",
+    "Zove se Gynaeceum – spratno odeljenje u kući, gde su odhranjivale decu i radile poslove.",
+    "Helenizam je počeo 323. g.p.n.e., posle smrti Aleksandra Velikog. Obuhvata period antičke istorije koji počinje osvajanjima Aleksandra III Velikog (Aleksandar Makedonski) 334.godine pre nove ere i traje oko tri veka, odnosno do pojave Rimskog Carstva.",
+    "Posvećen je Zevsu u čast pobede nad Galima. Sagradio ga je Eumen II na terasi južno od Atininog hrama.",
+    "Milonska Venera, Nika sa Samotrake, Laokoon i sinovi, Gal na umoru, Samoubistvo Gola, Zevsov žrtvenik.",
+    "Hramove, bazilike, akvadukte, palate, kuće (domusi i insule), teatre, amfiteatre, terme, slavoluke.",
+    "Trajanov stub podignut je u spomen Trajanovih uspeha u Dakiji, mermer, 29m viosk, plitki reljef koji prikazuje Trajanov prelazak Dunava kod Rame.",
+    "Panteon je rimski hram, posvećen svim bogovima. Sagrađen je u 2.v.n.e.",
+    "Terme ili javna kupatila bila su obično u sastavu velikih vila i sadržale su prostorije za kupanje, bazene sa hladnom, mlakom i toplom vodom, svlačionice i biblioteke. U Beogradu se nalaze na prostoru Studentskog trga, akademskog parka i Plato ispred filozofskog fakulteta, u vreme kada su živeli Rimljani, a grad se zvao Singidunum.",
+    "Nalazi se u Splitu.",
+    "Ara Pracis je oltar posvećen miru, zamišljen kao hram svih rimskih bogova. Podignut je u 13. v.p.n.e. na Marsovskom polju, u čast povratka cara iz Španije i Galije – Augustu Mirotrovcu.",
+    "Gema je ukrasni kamen sa urezanom predstavom. Dekoracija može biti ornamentna ili figurativna, ponekad i sa natpisom. Gema je korišćena kao pečat, prsten ili neki drugi deo nakita.",
+    "To su realistični portreti na drvetu koji su slikani u doba helenističkog Egipta. Njihova namena je bila da čuvaju sećanje na umrlog. U izradi je korišćena tehnika Enkaustike-bojeni vosak na drvenoj podlozi.",
+    "Sistemi vodovodnih cevi iz rimskog perioda. Postoje podzemni i nadzemni akvadukti. Kanali koji su služili za dostavljanje vode od izvora do rimskih gradova.",
+    "Konstantin",
+    "Jevrejska religija, vera u jednog boga, bestelesnog, isključivo duhovnog oca svih ljudi.",
+    "Antička sinagoga, u Siriji u Dura-Europosu. O izgradnji sinagoge svedoči aramejski natpis iz 244.godine što je čini najstarijom sinagogom na svetu. Njena struktura je gotovo netaknuta i unutar sinagoge su sačuvane brojne zidne slike. Iskopavanjima su takođe otkrivene veoma važne zidne slike iz bogomolje iz hrišćanskog perioda, crkve koja je postojala na mestu Dura Europasa.",
+    "Postoje, Beogradska sinagoga u ulici Maršala Tolbuhina.",
+    "Sarkofazi i slikarstvo katakombi.",
+    "Ranohrišćanska ikonografija, slikarstvo i freske u Dura Europosu.",
+    "To su podzemne prostorije gde su progonjeni vernici spojili dve svoje potrebe – za ispovedanje vere i sahranjivanje svojih mrtvih u adekvatne grobnice. Sa strana tesnih hodnika nalazili su se useci gde su smeštani pogrebni ostaci i zatvarani pogrebnim pločama.",
+    "To je način na koji su vizantinci definisali prostor i materijalno okruženje. Ona govori o poimanju boga, religija, svesti uopšte, vlasti, moći, monaštva. Primere nalazimo na ulicama, trgovima Carigrada, na monaškim svetim gorama, mestima hodočašća. Slika kaodeo vizantijske vizuelne kulture zastupljena je na novcu, u sklopu religioznih obreda, ikona pri krštenju, venčanju, dnevnoj molitvi, nekoj javnoj ceremoniji.",
+    "330. g.p.n.e. – 1453. G.n.e.",
+    "Crkva Svete Sofije u Istanbulu je simbol vizantijske arhitekture. Nastala je u vreme cara Justinijana, kao delo Anatemija iz Trala i Isidora iz Mileta.",
+    "Na sistemu pandantifa, odnosno sfernih trouglova.",
+    "Justinijan i Teodora",
+    "Ikonoklazam je religiozni pokret zabrane obožavanja slika i ikona. Lav II i Konstantin izdali su seriju dikata protiv obožavanja i posedovanja ikona, da bi zatim Konstantin VI krajem 8.veka uveo opet ikone kao predmet poštovanja.",
+    "Apsida je zadnji, polukružni deo oltara. Apsida simboliše mesto Hristovog rođenja ali i mesto gde je sahranjem. Časna trpeza je najvažniji deo oltarskog prostora na kome se priprema završni čin Evharistije. Simboliše Grob i Presto Gospodnji.",
+    "Njegov osnivač je Muhamed, bogati trgovac iz Meke. U svojoj 40.godini počeo je da ispoveda novu religiju, podstaknut božanskim vizijama. Smatra se da je religija zvanično nastala 610.godine.",
+    "Hodočašće se sastoji iz tri obeda: prvi-Svi hodočasnici najpre odlažu svoju odeću i oblače bele haljine kao znak jednakosti pred Bogom, a zatim obilaze 7 puta oko Ćabe, a svaki put poljube ili barem dotaknu crni kamen koji je uzidan u Ćabi; Drugi- 7 puta trče između dva brežuljka (Safne i Merne) što simbolizuje potragu Hadžere i njenog sina Išmaela; Treće- Hodočasnici odlaze na brdo Arafat, udaljeno nekoliko kilometara od meke, gde obavljaju obred stajanja pred licem Gospodnjim, stoje od podne do zalaska sunca i razmišljaju o Alahu posle čega se vraćaju u Meku gde završavaju hodočašće.",
+    "Islamska bogomolja, mesto klanjanja i zajedničke molitve muslimana.",
+    "Da, u minijaturama, ilustracijama i slikarstvu.",
+    "Niša u zidu džamije koja ukazuje na sveti kamen – kiblu, u kome se muslimani celog sveta okreću u molitvi.",
+    "Propovedaonica (prvi put zabeležena u Medini)",
+    "Tadž Mahal - Indija",
+    "Vezuje se za karla Velikog, nastaje u 8.veku.",
+    "Palatinska kapela u Ahenu.",
+    "Knjiga ima ulogu u promociji učenja, hrišćanstva i standardizacije crkvenog prava.",
+    "10,11.vek",
+    "Benediktanska manastirska crkva svetog Mihajla u Hildeshajmu.",
+    "11. i 12. Vek",
+    "Ekspanzija monaštva, zahvaljujući ekonomskom napretku, političkoj stabilnosti i porastu populacije.",
+    "Crkva posvećena Svetom Petru i Pavlu u Burgundiji. Danas su u ruševinama. ; Crkvu Svete Magdalene u vezleju; Crkva Svetog lazara u Otenu.",
+    "Katedrala kompleksa u Pizi.",
+    "To je umetnost iz perioda kasnog srednjeg veka, umetnički pravac koji je došao posle romanika kraj 12.veka u Francuskoj do kraja 15.veka u srednjoj i severnoj Evropi. Gotika je bila isključivo sakralna umetnost, koja je dala opipljivu težinu rastućoj moći crkve u Rimu.",
+    "Gotička katedrala predstavlja centar duhovnosti gde su se okupljali vernici čak i iz susednih regiona, da bi prisustvovali liturgiji.",
+    "Fresko slikarstvo, funerarna umetnost, nauka, knjige (istorija, filozofija, astronomija, algebra, medicina)",
+    "Đoto-Firenca; Dućo-Sijena.",
+    "Rotonda. Posvećena je Svetom Petru i Pavlu u blizini Novog Pazara. To je bilo srednjovekovno episkopsko sedište i saborno mesto srpskih vladara. Crkva je sagrađena u 8. ili 9. veku, sa napomenom da njeno jezgro čini ranohrišćanska građevina, verovatno iz 6.veka.",
+    "Studenica, Đurđevi stupovi, manastir presvete Bogorodice u Kuršumliji, Hilandar, manastir Svetog Nikole, Manastir Bogorodice Bistričke.",
+    "Miroslavljevo i Vukanovo jevanđelje.",
+    "Oko 1300. godine srpska umetnost u celini potpada pod dominantni uticaj Vizantije. Ulaskom u severnu Makedoniju, Srbi se izlažu snažnom uticaju čiste vizantijske umetnosti. Ponovio se stari proces: pobednici su bili pobeđeni jačom i starijom umetničkom kulturom koju su prihvatili i sa neočekivanom veštinom iskoristili za svoje potrebe.",
+    "Manastir Gračanica, Bogorodica Ljeviška, Manastir Svetog Đorđa.",
+    "Ljubostinja, Ravanica, Lazarica, Manasija, Kalenić",
+    "Stefan Dečanski"
+];
+
+
+let max;
+let br = 0;
+
+let brT = 0;
+let brN = 0;
+
+var q;
+var a;
+
+var index=0;
+
+function init(){
+    //var index = Math.round(Math.random()*max);
+    max = qq.length;
+    q = document.getElementById('q');
+    a = document.getElementById("a");
+    a.innerHTML=aa[index];
+    q.innerHTML=qq[index];
+
+    console.log(index);
+
+    document.getElementById('br').innerHTML=max;
+}
+
+function prikaziOdg(){
+    var a = document.getElementById('a');
+    a.classList.toggle('invisible');
+}
+
+function da(){
+    brT++;
+    br++;
+    dalje();
+}
+
+function ne(){
+    brN++;
+    br++;
+    dalje();
+}
+
+function dalje(){
+    //var index = Math.round(Math.random()*max);
+    index++;
+    a.innerHTML=aa[index];
+    q.innerHTML=qq[index];
+
+    console.log(index);
+
+    document.getElementById('good').innerHTML=brT;
+    document.getElementById('bad').innerHTML=brN;
+    document.getElementById('counter').innerHTML=br;
+    document.getElementById('br').innerHTML=max;
+}
